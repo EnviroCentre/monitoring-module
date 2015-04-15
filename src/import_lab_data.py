f7168c97faec14b8538f61d47dacdb22529fa1f5
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-15 20:19:36 +0100

    script skeletons

---
+++ src/import_lab_data.py
@@ -0,0 +1,9 @@
+# name=Import lab data
+# displayinmenu=true
+# displaytouser=true
+# displayinselector=true
+
+from hec.dssgui import ListSelection
+from hec.heclib.dss import *
+from hec.heclib.util import HecTime
+from hec.script import *
---
+++ src/monitoring_plots.py
@@ -0,0 +1,9 @@
+# name=Monitoring plots
+# displayinmenu=true
+# displaytouser=true
+# displayinselector=true
+
+from hec.dssgui import ListSelection
+from hec.heclib.dss import *
+from hec.heclib.util import HecTime
+from hec.script import *
