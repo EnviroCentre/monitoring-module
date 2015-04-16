0959b73aa02abddd05561e7d430a3eacaf265c26
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-16 19:07:30 +0100

    input validation

--- src/import_field_data.py
+++ src/import_field_data.py
@@ -4,6 +4,37 @@
 # displayinselector=true
 
 from hec.dssgui import ListSelection
-from hec.heclib.dss import *
-from hec.heclib.util import HecTime
-from hec.script import *
+from hec.script import MessageBox
+from monitoring import dataimport
+import os.path
+import yaml
+
+CONFIG_FILE = 'field_import.yml'
+DSS_FILE = ListSelection.getMainWindow().getDSSFilename()
+
+def validateConfig(config, errors=[]):
+    valid = 1
+    try:
+        pass
+    except:
+        valid = 0
+    return valid
+
+if not DSS_FILE:
+    MessageBox.showError("Please open an HEC-DSS database first.", "HEC-DSSVue")
+    raise ValueError("No HEC-DSS database file open.")
+
+configFilePath = os.path.join(os.path.dirname(DSS_FILE), CONFIG_FILE)
+if not os.path.isfile(configFilePath):
+    MessageBox.showError("The configuration file %s does not exist.\n\nPlease create this file and try again." % configFilePath, 
+                         "HEC-DSSVue")
+    raise ValueError("Cannot find config file %s" % configFilePath)
+
+config = yaml.load(open(configFilePath).read()).next()
+errors = []
+if validateConfig(config, errors):
+    dataimport.locationsAcross(config, DSS_FILE)
+else:
+    MessageBox.showError("The configuration file %s is not valid. Please check the content and try again." % configFilePath,
+                         "HEC-DSSVue")
+    raise errors
