835edfd47a15b5fc927fed55093d1acec224bd83
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-17 18:38:34 +0100

    add package to deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@ $appdataFolder = "$env:APPDATA\HEC\HEC-DSSVue"
 
 Copy-Item .\src\*.py -destination "$appdataFolder\scripts"
 
-$packages = "monitoring", "yaml"
+$packages = "monitoring", "yaml", "toolbox"
 
 ForEach ($package in $packages) {
     New-Item -type directory -force "$appdataFolder\lib\$package"
