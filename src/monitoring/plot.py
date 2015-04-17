6783dfe1a9560088c7659a8e47a8f0995653e21d
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-17 19:07:22 +0100

    set relative or abs output folder

--- src/monitoring/plot.py
+++ src/monitoring/plot.py
@@ -1,2 +1,12 @@
+import os
+from os import path
+
 def exportImages(config, dssFilePath):
-    pass
+    if path.isabs(config['output_folder']):
+        outputFolder = config['output_folder']
+        # assume folder exists if absolute path
+    else:
+        outputFolder = path.join(path.dirname(dssFilePath), config['output_folder'])
+        if not path.isdir(outputFolder):
+            # create relative folder if required
+            os.mkdir(outputFolder)
