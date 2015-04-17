ccd04fea7de30564f48418a0a330f75207ca58a9
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-17 21:20:46 +0100

    deploy packages to scripts folder

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ Copy-Item .\src\*.py -destination "$appdataFolder\scripts"
 $packages = "monitoring", "yaml", "toolbox"
 
 ForEach ($package in $packages) {
-    New-Item -type directory -force "$appdataFolder\lib\$package"
-    Remove-Item "$appdataFolder\lib\$package\*$py.class"
-    Copy-Item .\src\$package\* -destination "$appdataFolder\lib\$package" -recurse
+    New-Item -type directory -force "$appdataFolder\scripts\$package"
+    Remove-Item "$appdataFolder\scripts\$package\*$py.class"
+    Copy-Item .\src\$package\* -destination "$appdataFolder\scripts\$package" -recurse
 }
