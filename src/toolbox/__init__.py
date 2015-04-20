fc4021eece9a2b6b946f34d33246111e4dce8b50
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-20 21:54:46 +0100

    ValidationError

--- src/toolbox/__init__.py
+++ src/toolbox/__init__.py
@@ -1,8 +1,9 @@
+import codecs
 from hec.dssgui import ListSelection
 from hec.script import MessageBox
 from os import path
+from toolbox.util import ValidationError
 import yaml
-import codecs
 
 class Tool(object):
     requiredParams = []
@@ -24,28 +25,25 @@ class Tool(object):
     def _toolIsValid(self):
         # Check if HEC-DSS db exists
         if not path.isfile(self.dssFilePath):
-            error = ValueError("Please open a HEC-DSS database first.")
-            MessageBox.showError(str(error), "HEC-DSSVue")
+            error = ValidationError("Please open a HEC-DSS database first.")
+            MessageBox.showError(error.message, "HEC-DSSVue")
             raise error
         
         # Check if config file exists
         if not path.isfile(self.configFilePath):
-            error = ValueError("The configuration file %s does not exist.\n\nPlease create this file and try again." % self.configFilePath)
-            MessageBox.showError(str(error), "HEC-DSSVue")
+            error = ValidationError("The configuration file %s does not exist.\n\nPlease create this file and try again." % self.configFilePath)
+            MessageBox.showError(error.message, "HEC-DSSVue")
             raise error
         
         return 1
     
     def _configIsValid(self):
-        errors = []
-
-        for param in self.requiredParams:
-            if not param in self.config:
-                errors.append(ValueError("The parameter '%s' does not exist." % param))
+        errors = [ValidationError("The parameter '%s' does not exist." % param) 
+            for param in self.requiredParams if not param in self.config]
 
         if errors:
             self._displayConfigErrors(errors)
-            raise errors
+            raise ValidationError(errors)
         else:
             return 1
     
@@ -53,7 +51,7 @@ class Tool(object):
         message = "The configuration file %s is not valid.\nPlease check the content and try again." % self.configFilePath
         message += "\n"
         for error in errors:
-            message += "\n - %s" % error
+            message += "\n - %s" % error.message
         MessageBox.showError(message, "HEC-DSSVue")
 
     def run(self):
--- src/toolbox/util.py
+++ src/toolbox/util.py
@@ -17,3 +17,39 @@ def relativeFolder(folder, dssFilePath, createFolder='ifrelative'):
         if not path.isdir(absPath) and createFolder.lower() in ['allways', 'ifrelative']:
             os.mkdir(absPath)
     return absPath
+
+
+class ValidationError(Exception):
+    """
+    An error while validating data.
+    
+    Based django.core.exceptions.ValidationError
+    Copyright (c) Django Software Foundation and individual contributors. All rights reserved.
+    """
+    
+    def __init__(self, message):
+        """
+        The :arg:`message` argument can be a single error, a list of errors. What we define as an "error" can be either 
+        a simple string or an instance of ValidationError with its message attribute set, and what we define as
+        list can be an actual `list` or an instance of ValidationError with its `error_list` attribute set.
+        """
+
+        if isinstance(message, ValidationError):
+            message = message.message
+        
+        elif isinstance(message, list):
+            self.error_list = []
+            for message in message:
+                if not isinstance(message, ValidationError):
+                    message = ValidationError(message)
+                self.error_list.extend(message.error_list)
+        
+        else:
+            self.message = message
+            self.error_list = [self]
+            
+    def __str__(self):
+        message = ""
+        for error in self.error_list:
+            message += "ValidationError(%s)\n" % error.message
+        return message
