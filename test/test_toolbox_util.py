ce958000f9bbe503ce33c00059f75bf3c63c4958
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-04-25 20:59:33 +0100

    date time parsing test

--- test/test_toolbox_util.py
+++ test/test_toolbox_util.py
@@ -22,7 +22,22 @@ class ToolboxUtilTestCase(unittest.TestCase):
     def test_parseMeasurementBelowLODSpaces(self):
         result = toolbox.util.parseMeasurement(" < 2.0 ")
         self.assertEqual(result, 1.0)
+        
+    def test_dateDefaultFormat(self):
+        dt = toolbox.util.parseDateAndTime("2015/12/31", "01:00:00")
+        self.assertEqual(dt.year(), 2015)
+        self.assertEqual(dt.month(), 12)
+        self.assertEqual(dt.day(), 31)
+        self.assertEqual(dt.hour(), 1)
+        self.assertEqual(dt.minute(), 0)
+
+    def test_dateyyyymmddFormat(self):
+        dt = toolbox.util.parseDateAndTime("2015/12/31", "01:00:00", "yyyy/mm/dd")
+        self.assertEqual(dt.year(), 2015)
+        self.assertEqual(dt.month(), 12)
+        self.assertEqual(dt.day(), 31)
+        self.assertEqual(dt.hour(), 1)
+        self.assertEqual(dt.minute(), 0)
 
 if __name__ == '__main__':
     unittest.main()
-
