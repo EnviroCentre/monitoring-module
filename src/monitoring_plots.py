d70db48bd54b0639e03fd7973670e158fbe2d3f3
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-05-11 20:32:48 +0100

    use schema in plot config

--- src/monitoring_plots.py
+++ src/monitoring_plots.py
@@ -5,11 +5,38 @@
 
 from monitoring import plot
 import toolbox as tb
+from voluptuous import Schema, All, Any, Range, Datetime
 
 
 class PlotTool(tb.Tool):
-    requiredParams = ['site', 'locations', 'interval', 'version', 'period', 
-        'params', 'width', 'height', 'line', 'output_folder']
+    schema = Schema({
+        'site': unicode,
+        'locations': [
+            unicode
+        ],
+        'interval': unicode,
+        'version': unicode,
+        'output_folder': unicode,
+        'period': {
+            'start': Datetime("%d%b%Y %H:%M", msg="Start date must be formatted like this: 01JAN2000 00:00"),
+            'end':   Datetime("%d%b%Y %H:%M", msg="End date must be formatted like this: 01JAN2000 00:00")
+        },
+        'params': {
+            unicode: {
+                'scale': Any('lin', 'log')
+            }
+        },
+        'width':  All(int, Range(min=100, max=3000)),
+        'height': All(int, Range(min=100, max=3000)),
+        'line': {
+            'width': All(float, Range(min=0.5, max=2.0)),
+            'colours': [
+                [All(int, Range(min=0, max=255)),
+                 All(int, Range(min=0, max=255)),
+                 All(int, Range(min=0, max=255))]
+            ]
+        }
+    }, required=True)
     
     def main(self):
         plotted, messages = plot.onePerParam(self.config, self.dssFilePath)
