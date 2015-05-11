58c1fec5d104ee908cdd4dabe7065eac73983a24
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-05-11 07:03:14 +0100

    string interpolation method

--- src/import_lab_data.py
+++ src/import_lab_data.py
@@ -15,7 +15,7 @@ class ImportTool(tb.Tool):
     def main(self):
         records = importdata.locationsAcross(self.config)
         imported = tbu.saveIrregularRecords(records, self.dssFilePath)
-        self.message = "%s Records imported." % imported
+        self.message = "{} Records imported.".format(imported)
 
 
 tool = ImportTool()
