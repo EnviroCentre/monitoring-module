15f4b57657e9031f803b111c7f37a39d3f6ebba0
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-05-11 07:01:45 +0100

    required config keys

--- src/import_field_data.py
+++ src/import_field_data.py
@@ -9,13 +9,14 @@ import toolbox.util as tbu
 
 
 class ImportTool(tb.Tool):
-    requiredParams = ['folder', 'files', 'site', 'version', 'params', 'columns']
+    requiredParams = ['folder', 'files', 'site', 'version', 'columns', 
+        'mapping', 'params']
     refreshCatalogue = 1
     
     def main(self):
         records = importdata.locationsDown(self.config)
         imported = tbu.saveIrregularRecords(records, self.dssFilePath)
-        self.message = "%s Records imported." % imported
+        self.message = "{} Records imported.".format(imported)
 
 
 tool = ImportTool()
