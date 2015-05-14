e0415dafb277675d3cdb5885741660f29d1e1f14
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-05-14 21:31:02 +0100

    Record object

---
+++ src/monitoring/__init__.py
@@ -0,0 +1,63 @@
+import collections
+
+
+class Record(object):
+    def __init__(self, site=None, location=None, parameter=None, version=None,
+                 units="-", startTime=None, interval=-1, values=[]):
+        self.site = site.upper()
+        self.location = location.upper()
+        self.parameter = parameter.upper()
+        self.version = version.upper()
+        self.units = units
+        self.startTime = startTime
+        self.interval = interval
+        self.type = "INST-VAL"
+        if isinstance(values, collections.Sequence):
+            self._values = values
+        else:
+            self._values = [values]
+    
+    @property
+    def origin(self):
+        if len(self._values) == 1:
+            return "sample"
+        elif len(self._values) > 1:
+            return "logger"
+        else:
+            return None
+
+    @property
+    def values(self):
+        return self._values
+
+    @values.setter
+    def values(self, v):
+        if isinstance(v, collections.Sequence):
+            self._values = v
+        else:
+            self._values = [v]
+            
+    @property
+    def fullName(self):
+        return "{0.site}/{0.location}/{0.parameter}//{0.interval:d}MIN/{0.version}".format(self)
+    
+    @property
+    def times(self):
+        if len(self) > 1:
+            return range(self.startTime, 
+                         self.startTime + len(self) * self.interval, 
+                         self.interval)
+        elif len(self) == 1:
+            return [self.start]
+        else:
+            return []
+        
+    @property
+    def endTime(self):
+        return self.times[-1]
+        
+    def __len__(self):
+        return len(self._values)
+        
+    def __repr__(self):
+        return "Record: {0.origin} data at {0.location}".format(self)
